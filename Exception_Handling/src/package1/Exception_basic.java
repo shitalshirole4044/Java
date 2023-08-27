128f8cb209de6ec7463c14c8183facc3e9052abf
shital shirole <shitalshirole4044@gmail.com>
2023-08-27 05:43:36 -0700

    Exception_handling

---
+++ Exception_Handling/src/package1/Exception_basic.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class Exception_basic {
+	void m1() {
+		System.out.println("in m1");
+	}
+	void m2() {
+		System.out.println("in m2");
+	}
+	public static void main(String[] args) {
+		Exception_basic obj=new Exception_basic();
+		obj.m1();
+		
+		}
+}
